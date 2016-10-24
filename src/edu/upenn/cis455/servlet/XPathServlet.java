7510afdefd3289c51075609c61c4c995de444aab
PiotrJander <pjander2@gmail.com>
2016-10-24 13:59:53 -0400

    A lot changed

--- src/edu/upenn/cis455/servlet/XPathServlet.java
+++ src/edu/upenn/cis455/servlet/XPathServlet.java
@@ -1,15 +1,30 @@
 package edu.upenn.cis455.servlet;
 
+import com.sleepycat.je.DatabaseException;
+import edu.upenn.cis455.storage.DBWrapper;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @SuppressWarnings("serial")
 public class XPathServlet extends HttpServlet {
-	
-	/* TODO: Implement user interface for XPath engine here */
-	
-	/* You may want to override one or both of the following methods */
 
-	@Override
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        try {
+            super.init(config);
+            String envDirPath = config.getServletContext().getInitParameter("BDBstore");
+            DBWrapper.init(new File(envDirPath));
+        } catch (DatabaseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
 	public void doPost(HttpServletRequest request, HttpServletResponse response) {
 		/* TODO: Implement user interface for XPath engine here */
 
@@ -18,10 +33,40 @@ public class XPathServlet extends HttpServlet {
 
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) {
-		/* TODO: Implement user interface for XPath engine here */
+        if (request.getSession().getAttribute("username") == null) {
+            // not logged
+            loginForm(request, response);
+        } else {
+            homePage(request, response);
+        }
 
-		
-	}
+    }
+
+    private void homePage(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            PrintWriter writer = response.getWriter();
+            writer.println("<html><head><title>Home page</title></head><body>");
+            writer.println("User name: " + request.getSession().getAttribute("username"));
+            writer.println("</body></html>");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loginForm(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            PrintWriter writer = response.getWriter();
+            writer.println("<html><head><title>Home page</title></head><body>");
+            writer.println("<form action=\"register.jsp\" method=\"post\">");
+            writer.println("<p>Username <input type=\"text\" name=\"username\"></p>");
+            writer.println("<p>Password <input type=\"text\" name=\"password\"></p>");
+            writer.println("<p><input type=\"submit\"></p>");
+            writer.println("</form>");
+            writer.println("</body></html>");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
 
--- src/edu/upenn/cis455/storage/DBWrapper.java
+++ src/edu/upenn/cis455/storage/DBWrapper.java
@@ -1,15 +1,69 @@
 package edu.upenn.cis455.storage;
 
+import com.sleepycat.je.DatabaseException;
 import com.sleepycat.je.Environment;
+import com.sleepycat.je.EnvironmentConfig;
 import com.sleepycat.persist.EntityStore;
+import com.sleepycat.persist.PrimaryIndex;
+import com.sleepycat.persist.StoreConfig;
+import com.sleepycat.persist.model.Entity;
+import com.sleepycat.persist.model.PrimaryKey;
+
+import java.io.File;
 
 public class DBWrapper {
 	
 	private static String envDirectory = null;
 	
-	private static Environment myEnv;
+	private static Environment env;
 	private static EntityStore store;
+    private static UserAccessor dao;
 	
 	/* TODO: write object store wrapper for BerkeleyDB */
+
+    public static void init(File envHome) throws DatabaseException {
+
+        /* Open a transactional Berkeley DB engine environment. */
+        EnvironmentConfig envConfig = new EnvironmentConfig();
+//        envConfig.setAllowCreate(true);
+//        envConfig.setTransactional(true);
+        env = new Environment(envHome, envConfig);
+
+        /* Open a transactional entity store. */
+        StoreConfig storeConfig = new StoreConfig();
+//        storeConfig.setAllowCreate(true);
+//        storeConfig.setTransactional(true);
+        store = new EntityStore(env, "UserStore", storeConfig);
+
+        /* Initialize the data access object. */
+        dao = new UserAccessor(store);
+    }
+
+    /* An entity class. */
+    @Entity
+    static class User {
+
+        @PrimaryKey
+        String username;
+
+        String password;
+
+        public User(String username, String password) {
+            this.username = username;
+            this.password = password;
+        }
+
+        private User() {} // For deserialization
+    }
+
+    /* The data accessor class for the entity model. */
+    static class UserAccessor {
+
+        PrimaryIndex<String,User> userByUsername;
+
+        public UserAccessor(EntityStore store) throws DatabaseException {
+            userByUsername = store.getPrimaryIndex(String.class, User.class);
+        }
+    }
 	
 }
