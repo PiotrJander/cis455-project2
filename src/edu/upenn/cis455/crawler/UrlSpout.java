fd310c3c896eb04e2828a6c3ffae0f4110408ae3
PiotrJander <pjander2@gmail.com>
2016-10-31 22:45:24 -0400

    Impls nextTuple for UrlSpout

--- src/edu/upenn/cis455/crawler/UrlSpout.java
+++ src/edu/upenn/cis455/crawler/UrlSpout.java
@@ -6,16 +6,19 @@ import edu.upenn.cis.stormlite.routers.IStreamRouter;
 import edu.upenn.cis.stormlite.spout.IRichSpout;
 import edu.upenn.cis.stormlite.spout.SpoutOutputCollector;
 import edu.upenn.cis.stormlite.tuple.Fields;
+import edu.upenn.cis.stormlite.tuple.Values;
+import edu.upenn.cis455.UrlFrontier;
 import org.apache.log4j.Logger;
 
+import java.net.URL;
 import java.util.Map;
 import java.util.UUID;
 
 public class UrlSpout implements IRichSpout {
 
-    static Logger log = Logger.getLogger(UrlSpout.class);
-    String executorId = UUID.randomUUID().toString();
-    SpoutOutputCollector collector;
+    private static Logger log = Logger.getLogger(UrlSpout.class);
+    private String executorId = UUID.randomUUID().toString();
+    private SpoutOutputCollector collector;
 
     @Override
     public String getExecutorId() {
@@ -30,26 +33,23 @@ public class UrlSpout implements IRichSpout {
     @Override
     public void open(Map<String, String> config, TopologyContext topo, SpoutOutputCollector collector) {
         this.collector = collector;
-
-        // TODO
-
-//        try {
-//            log.debug(getExecutorId() + " opening file reader");
-//            reader = new BufferedReader(new FileReader("words.txt"));
-//        } catch (FileNotFoundException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
     }
 
     @Override
     public void close() {
-        // TODO
+        // Do nothing
     }
 
     @Override
     public void nextTuple() {
-        // TODO
+        URL url = UrlFrontier.INSTANCE.poll();
+        if (url != null) {
+            log.debug(getExecutorId() + " emitting " + url);
+            this.collector.emit(new Values<>(url));
+        }
+
+        // TODO do I need this?
+        // Thread.yield();
     }
 
     @Override
